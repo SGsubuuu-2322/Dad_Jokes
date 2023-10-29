eb123f1ac6892318ae8b88362cc6169329c5c86c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-30 01:07:45 +0530

    Added scripts for fetching jokes through JS...

---
+++ script.js
@@ -0,0 +1,20 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", fetchJokes);
+
+fetchJokes();
+
+function fetchJokes() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+}
